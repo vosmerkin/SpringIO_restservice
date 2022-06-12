a8e882af49168e1ec9d4b16fbd2d4258cf7d411e
Yevhen Melnyk <vosmerkin.evgen@gmail.com>
2022-06-12 20:34:49 +0300

    ConditionalOnProperties

--- src/main/java/com/example/restservice/config/GreetConfig.java
+++ src/main/java/com/example/restservice/config/GreetConfig.java
@@ -3,18 +3,24 @@ package com.example.restservice.config;
 import com.example.restservice.greeting.GreetManagerCongrat;
 import com.example.restservice.greeting.GreetManager;
 import com.example.restservice.greeting.GreetManagerImpl;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 
 @Configuration
 public class GreetConfig {
+    @ConditionalOnProperty(name = "spring.greeting.manager",
+            havingValue = "default",
+            matchIfMissing = true)
     @Bean
-    public GreetManager greetManagerImpl(){
+    public GreetManager greetManagerImpl() {
         return new GreetManagerImpl();
     }
+
+    @ConditionalOnProperty(name = "spring.greeting.manager", havingValue = "greeting")
     @Bean
-    public GreetManager greetManagerCongrat(){
+    public GreetManager greetManagerCongrat() {
         return new GreetManagerCongrat();
     }
 }
